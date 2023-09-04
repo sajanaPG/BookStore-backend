9f237734a7396d91bff365297a2d5d8a43e9660a
Sajana <sajanapeshitha@gmail.com>
2023-09-04 12:03:38 +0530

    first commit

---
+++ src/test/java/com/ijse/bookStore/BookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ijse.bookStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
